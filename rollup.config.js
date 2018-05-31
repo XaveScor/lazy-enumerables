edc04d4184b08ec30394d23b72d51ac2aa4db32e
Andrey Vassilyev <XaveScor@gmail.com>
2018-05-31 22:11:22 +0300

    es modules build

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
+/* global __dirname */
 import path from 'path'
 
 export default {
-    input: path.resolve(__dirname, 'index.js'),
-    output: {
-        name: 'build.js',
-        file: path.resolve(__dirname, 'dist', 'index.js'),
-        format: 'umd'
-    }
+	input: path.resolve(__dirname, 'index.js'),
+	output: {
+		name: 'build.js',
+		file: path.resolve(__dirname, 'dist', 'index.js'),
+		format: 'es',
+	},
 }
