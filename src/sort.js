3c630476a26d49a0a62faecaa5188ed0982e537f
Andrey Vassilyev <XaveScor@gmail.com>
2018-06-10 02:47:33 +0300

    increase sort perfomance

--- src/sort.js
+++ src/sort.js
@@ -2,29 +2,29 @@ import {toArray} from './toArray'
 
 const naturalOrderComparator = (a1, a2) => a1 < a2
 
-function* bubbleSort(comp, arr) {
-	const n = arr.length
-	for (let i = 1; i < n; ++i) {
-		for (let r = 0; r < n - i; ++r) {
+function bubbleSort(comp, arr, left, right) {
+	let temp = null
+	for (let i = left + 1; i < right; ++i) {
+		for (let r = left; r < right - i; ++r) {
 			if (!comp(arr[r], arr[r + 1])) {
-				const temp = arr[r]
+				temp = arr[r]
 				arr[r] = arr[r + 1]
 				arr[r + 1] = temp
 			}
 		}
 	}
-	yield* arr
 }
 
-function split(comp, arr, splitPoint) {
-	let i = 0
-	let j = arr.length - 1
+function split(comp, arr, splitPoint, left, right) {
+	let i = left
+	let j = right - 1
 	const p = arr[splitPoint]
+	let temp
 	do {
-		while (comp(arr[i], p) && i < arr.length) i++
-		while (comp(p, arr[j]) && j >= 0) j--
+		while (comp(arr[i], p) && i < right) i++
+		while (comp(p, arr[j]) && j >= left) j--
 		if (i < j) {
-			const temp = arr[i]
+			temp = arr[i]
 			arr[i] = arr[j]
 			arr[j] = temp
 			i++
@@ -33,16 +33,28 @@ function split(comp, arr, splitPoint) {
 	} while (i < j)
 }
 
+function* slice(arr, left, right) {
+	for (let i = left; i < right; ++i) {
+		yield arr[i]
+	}
+}
+
+function* mutableSort(arr, comp, left, right) {
+	if (right - left < 10) {
+		bubbleSort(comp, arr, left, right)
+		yield* slice(arr, left, right)
+		return
+	}
+	const splitPoint = Math.floor(left + (right - left) / 2)
+	split(comp, arr, splitPoint, left, right)
+	yield* mutableSort(arr, comp, left, splitPoint)
+	yield arr[splitPoint]
+	yield* mutableSort(arr, comp, splitPoint + 1, right)
+}
+
 export function sort(comp = naturalOrderComparator) {
 	return function*(enumerable) {
 		const arr = Array.isArray(enumerable) ? enumerable : toArray(enumerable)
-		if (arr.length < 10) {
-			yield* bubbleSort(comp, arr)
-			return
-		}
-		const splitPoint = Math.floor(arr.length / 2)
-		split(comp, arr, splitPoint)
-		yield* sort(comp)(arr.slice(0, splitPoint))
-		yield* sort(comp)(arr.slice(splitPoint, arr.length))
+		yield* mutableSort(arr, comp, 0, arr.length)
 	}
 }
