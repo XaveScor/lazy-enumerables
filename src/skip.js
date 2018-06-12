79820ce68dd2cc0b9fcbb82466dea2db898ae269
Andrey Vassilyev <XaveScor@gmail.com>
2018-06-12 14:00:12 +0300

    optimize skip

--- src/skip.js
+++ src/skip.js
@@ -1,17 +1,10 @@
-import {lazyIterator} from './lazyIterator'
-
 export function skip(n) {
 	return function*(enumerable) {
-		if (!enumerable.next) {
-			return lazyIterator(enumerable)(skip(n))
-		}
-		let done = false
-		for (let i = 0; i < n && !done; ++i) {
+		for (let i = 0; i < n; ++i) {
 			const el = enumerable.next()
-			done = el.done
-		}
-		if (done) {
-			return
+			if (el.done) {
+				return
+			}
 		}
 		yield* enumerable
 	}
